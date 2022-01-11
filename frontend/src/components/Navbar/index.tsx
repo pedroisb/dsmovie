5a1f5a7874c67ba3a31cf22538d1e899eeef5f76
Pedro Brito <pedroisb28@gmail.com>
2022-01-11 13:02:44 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg'; //Note que Nélio a importou como componente e não a armazenando em uma variável
+import './styles.css';
+
+function Navbar() {
+
+    return (
+        <header>
+        <nav className="container">
+            <div className="dsmovie-nav-content">
+                <h1>DSMovie</h1>
+                <a href="https://github.com/pedroisb">
+                    <div className='dsmovie-contact-container'>
+                        <GithubIcon />
+                        <p className='ds-movie-contact-link'>/pedroisb</p>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </header>
+    );
+}
+
+export default Navbar;
