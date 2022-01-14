29b979c2c82d3a94c23bd094f612aef2e6d7fbb0
Pedro Brito <pedroisb28@gmail.com>
2022-01-14 13:07:55 -0300

    First request

---
+++ frontend/src/types/movie.ts
@@ -0,0 +1,22 @@
+export type Movie = {
+    id: number;
+    title: string;
+    score: number;
+    count: number;
+    image: string;
+}
+
+export type MoviePage = {
+    content: Movie[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size: number;
+    number: number;
+    first: boolean;
+    numberOfElements: number;
+    empty: boolean;
+}
+
+
+// as estruturas de Movie e de MoviePage podem ser obtidas a partir do objeto retornado pela API (basta checar no Postman)
